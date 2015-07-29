f09222ba1dd752ba8fa71c60c6fb78b947d75073
cd01220 <10861479@qq.com>
2015-07-29 10:06:20 +0800

    add code about how to use LibXml2

--- Codes/Main.cpp
+++ Codes/Main.cpp
@@ -3,14 +3,13 @@
 #include "Debug.h"
 #include "SystemError.h" 
 #include "TestItorator.h"
+#include "MyLibXml2.h"
 #include "Main.h"
 
 using namespace std;
 
-
 int main()
 {
-
-
+    TraverseXmlXpathNode();
 	return 0;
 }
